f0ed1279307f3a8a1a374acd53dc1a3f25642bb0
claraxfee <claraxfee@gmail.com>
2025-05-03 15:21:46 -0400

    full run.sh template

--- run.sh
+++ run.sh
@@ -1,18 +1,19 @@
-class=BankAccount
-project_cp=.
+class=org.apache.commons.math3.util.MathUtils
+project_cp= #TODO: REPLACE THIS WITH THE PATH RETURNED BY RUNNING: defects4j  export -p cp.compile -w "<path to check-out buggy code>" 2>/dev/null 
 D4J_SEED=42
 budget=600
 DIR_OUTPUT=out/
 
+
 java -cp evosuite-TAKE-1.jar shaded.org.evosuite.EvoSuite  \
-    -class $class \
-    -projectCP $project_cp \
-    -seed $D4J_SEED \
-    -Dsearch_budget=$budget \
-    -Dtest_dir=$DIR_OUTPUT \
-    -criterion branch \
-    -Dshow_progress=false \
-    -Djunit_check=false \
-    -Dfilter_assertions=false \
-    -Dtest_comments=false \
-    -mem 1500
+   -class $class \
+   -projectCP $project_cp \
+   -seed $D4J_SEED \
+   -Dsearch_budget=$budget \
+   -Dtest_dir=$DIR_OUTPUT \
+   -criterion branch \
+   -Dshow_progress=false \
+   -Djunit_check=false \
+   -Dfilter_assertions=false \
+   -Dtest_comments=false \
+   -mem 1500
